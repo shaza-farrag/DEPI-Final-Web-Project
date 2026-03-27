80bad3d01e9119c15d5e7ed37b7093d60d4977da
shaza-farrag <sw1190@fayoum.edu.eg>
2026-03-27 18:04:34 +0200

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,7 @@ function App() {
     //   <Route path="/" element={<Home />} />
     //   <Route path="/login" element={<Login />} />
     //   <Route path="/dashboard" element={<Dashboard />} />
-    // </Routes>
-
+    // helloooooooo
     <Footer />
   )
 }
---
+++ frontend/src/components/cart_overlay/Cart_overlay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cart_overlay() {
+  return (
+    <div>Cart_overlay</div>
+  )
+}
+
+export default Cart_overlay
