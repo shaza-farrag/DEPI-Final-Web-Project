bea6f69327eb3b5a7f0140d55aa81dda71efb32c
shaza-farrag <sw1190@fayoum.edu.eg>
2026-03-28 22:21:17 +0200

    Start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import {Routes , Route} from 'react-router-dom'
-import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Dashboard from './pages/dashboard/Dashboard'
+import About from './pages/about us/About'
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Home />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/dashboard" element={<Dashboard />} />
-    // helloooooooo
-    <Footer />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
