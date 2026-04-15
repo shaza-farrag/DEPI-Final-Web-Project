5dda8f47e239f7ddcb9eb1f9bdfe2e9b3de9a532
shaza-farrag <sw1190@fayoum.edu.eg>
2026-04-15 20:42:29 +0200

    Backend Update

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is working");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+})
